91068459047d0d3ceb5a0625952ac7a043e178fc
conchan-akita <ymrta.kws+git@gmail.com>
2018-07-16 19:52:21 +0900

    AddCommandクラスの別ファイル化

--- CalculationProject/CalculationProject/MainWindowViewModel.cs
+++ CalculationProject/CalculationProject/MainWindowViewModel.cs
@@ -29,28 +29,4 @@ namespace CalculationProject
             }
         }
     }
-    public class AddCommand : ICommand
-    {
-        public MainWindowViewModel _mainWindowViewModel;
-        public AddCommand(MainWindowViewModel mainWindowViewModel)
-        {
-            _mainWindowViewModel = mainWindowViewModel;
-        }
-
-        public event EventHandler CanExecuteChanged;
-
-        public bool CanExecute(object parameter)
-        {
-            // TODO: Impl.
-            return true;
-            //throw new NotImplementedException();
-        }
-
-        public void Execute(object parameter)
-        {
-            _mainWindowViewModel.Z = _mainWindowViewModel.X + _mainWindowViewModel.Y;
-            _mainWindowViewModel.OnPropertyChanged("Z");
-            //throw new NotImplementedException();
-        }
-    }
 }
